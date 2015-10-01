10399b1586e84288c6495f84284d077f63c7cfc9
Cyrus <c@mailcyr.us>
2015-10-01 00:19:05 -0400

    First commit.

---
+++ hello-world-server.js
@@ -0,0 +1,25 @@
+/*var http = require('http');
+var web3 = require('web3');
+http.createServer(function handler(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('something\n');
+    web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
+    res.write('' + web3.fromWei(web3.eth.getBalance('0xcf684dfb8304729355b58315e8019b1aa2ad1bac')) + '\n');
+    res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
+*/
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World! express');
+});
+
+var server = app.listen(1337, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
