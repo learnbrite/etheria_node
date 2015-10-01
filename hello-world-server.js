35819b531387fce42f590ccdbddda9d277386c2c
Cyrus <c@mailcyr.us>
2015-10-01 14:09:03 -0400

    nc

--- hello-world-server.js
+++ hello-world-server.js
@@ -11,10 +11,39 @@ console.log('Server running at http://127.0.0.1:1337/');
 */
 
 var express = require('express');
+
+var web3 = require('web3');
+web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
+
+var abi = [{"constant":true,"inputs":[],"name":"getElevations","outputs":[{"name":"","type":"uint8[33][33]"}],"type":"function"},{"constant":false,"inputs":[{"name":"x","type":"uint8"},{"name":"y","type":"uint8"},{"name":"block","type":"int8[7]"}],"name":"addBlock","outputs":[],"type":"function"},{"constant":false,"inputs":[],"name":"kill","outputs":[],"type":"function"},{"constant":false,"inputs":[{"name":"x","type":"uint8"},{"name":"y","type":"uint8"},{"name":"newowner","type":"address"}],"name":"setOwner","outputs":[],"type":"function"},{"constant":false,"inputs":[{"name":"rows","type":"uint8[]"}],"name":"initializeOwners","outputs":[],"type":"function"},{"constant":true,"inputs":[],"name":"getOwners","outputs":[{"name":"","type":"address[33][33]"}],"type":"function"},{"constant":true,"inputs":[{"name":"x","type":"uint8"},{"name":"y","type":"uint8"}],"name":"getBlocksForTile","outputs":[{"name":"","type":"int8[]"}],"type":"function"},{"constant":true,"inputs":[],"name":"getPrices","outputs":[{"name":"","type":"uint256[33][33]"}],"type":"function"},{"constant":false,"inputs":[{"name":"row","type":"uint8"},{"name":"_elevations","type":"uint8[33]"}],"name":"setElevations","outputs":[],"type":"function"},{"inputs":[],"type":"constructor"}];
+var contract = web3.eth.contract(abi).at("0x915f6a8fac7e4de205b72aa30289354921cbeb90");
+
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World! express');
+/* 
+ We want the map in this format:
+ {
+ 	"tiles":[
+ 		[tile0, ... tile32], [tile33...tile65]...
+ 	]
+ }
+ where each tile is 
+ {
+ 	"elevation": 134,
+ 	"owner": 0xabc123...,
+ 	"price": 7000000000
+ 	"blocks": [
+ 		[which0,blockx0,blocky0,blockz0,r0,g0,b0,which1,blockx1,blocky1,blockz1,r1,g1,b1,...]
+ 	]
+ }
+ */
+
+app.get('/map', function (req, res) {
+	//var resstr = 'Hello World! express2\n' + web3.fromWei(web3.eth.getBalance('0xcf684dfb8304729355b58315e8019b1aa2ad1bac')) + '\n';
+	var elevations = contract.getElevations();
+	var owners = contract.getOwners();
+	var prices = contract.getPrices();
+	res.json(contract.getElevations());
 });
 
 var server = app.listen(1337, function () {
