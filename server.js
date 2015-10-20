120abac572b25bdf7a4f9373ff1e0689d3651bec
cyrus <c@mailcyr.us>
2015-10-20 09:50:13 -0400

    Got log events working. Now we're cooking with gas!

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 var express = require('express');
 
 var web3 = require('web3');
-web3.setProvider(new web3.providers.HttpProvider('http://10.0.0.19:8545'));
+web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 
-//var abi = [{"constant":true,"inputs":[],"name":"getElevations","outputs":[{"name":"","type":"uint8[17][17]"}],"type":"function"},{"constant":true,"inputs":[],"name":"getIlliquidBalance","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"x","type":"uint8"},{"name":"y","type":"uint8"}],"name":"buyTile","outputs":[],"type":"function"},{"constant":false,"inputs":[],"name":"kill","outputs":[],"type":"function"},{"constant":true,"inputs":[{"name":"_b32","type":"bytes32"},{"name":"byteindex","type":"uint8"}],"name":"getUint8FromByte32","outputs":[{"name":"","type":"uint8"}],"type":"function"},{"constant":false,"inputs":[{"name":"row","type":"uint8"},{"name":"_elevations","type":"uint8[17]"}],"name":"initializeTiles","outputs":[],"type":"function"},{"constant":false,"inputs":[{"name":"x","type":"uint8"},{"name":"y","type":"uint8"}],"name":"makeOffer","outputs":[],"type":"function"},{"constant":true,"inputs":[{"name":"x","type":"uint8"},{"name":"y","type":"uint8"}],"name":"getOfferers","outputs":[{"name":"","type":"address[]"}],"type":"function"},{"constant":false,"inputs":[{"name":"x","type":"uint8"},{"name":"y","type":"uint8"},{"name":"indexOfBlockToEdit","type":"uint256"},{"name":"block","type":"int8[7]"}],"name":"editBlock","outputs":[],"type":"function"},{"constant":false,"inputs":[],"name":"retrieveLiquidBalance","outputs":[],"type":"function"},{"constant":true,"inputs":[],"name":"getLiquidBalance","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"x","type":"uint8"},{"name":"y","type":"uint8"}],"name":"farmTile","outputs":[],"type":"function"},{"constant":true,"inputs":[],"name":"getOwners","outputs":[{"name":"","type":"address[17][17]"}],"type":"function"},{"constant":true,"inputs":[{"name":"x","type":"uint8"},{"name":"y","type":"uint8"}],"name":"getBlocksForTile","outputs":[{"name":"","type":"int8[]"}],"type":"function"},{"constant":false,"inputs":[{"name":"x","type":"uint8"},{"name":"y","type":"uint8"},{"name":"i","type":"uint8"}],"name":"acceptOffer","outputs":[],"type":"function"},{"constant":false,"inputs":[{"name":"x","type":"uint8"},{"name":"y","type":"uint8"},{"name":"i","type":"uint8"}],"name":"rejectOffer","outputs":[],"type":"function"},{"constant":true,"inputs":[{"name":"x","type":"uint8"},{"name":"y","type":"uint8"}],"name":"getOffers","outputs":[{"name":"","type":"uint256[]"}],"type":"function"},{"constant":false,"inputs":[{"name":"x","type":"uint8"},{"name":"y","type":"uint8"}],"name":"retractOffer","outputs":[],"type":"function"},{"inputs":[],"type":"constructor"}];
-var abi = [{"constant":true,"inputs":[],"name":"getWhatHappened","outputs":[{"name":"","type":"uint8"}],"type":"function"},{"constant":true,"inputs":[{"name":"_b32","type":"bytes32"},{"name":"byteindex","type":"uint8"}],"name":"getUint8FromByte32","outputs":[{"name":"","type":"uint8"}],"type":"function"},{"constant":false,"inputs":[{"name":"col","type":"uint8"},{"name":"row","type":"uint8"},{"name":"_s","type":"string"}],"name":"setStatus","outputs":[],"type":"function"},{"constant":false,"inputs":[{"name":"col","type":"uint8"},{"name":"row","type":"uint8"}],"name":"makeOffer","outputs":[],"type":"function"},{"constant":true,"inputs":[{"name":"col","type":"uint8"},{"name":"row","type":"uint8"}],"name":"getOfferers","outputs":[{"name":"","type":"address[]"}],"type":"function"},{"constant":false,"inputs":[{"name":"col","type":"uint8"},{"name":"row","type":"uint8"},{"name":"index","type":"uint256"},{"name":"_block","type":"int8[5]"}],"name":"editBlock","outputs":[],"type":"function"},{"constant":false,"inputs":[{"name":"col","type":"uint8"},{"name":"row","type":"uint8"},{"name":"_n","type":"string"}],"name":"setName","outputs":[],"type":"function"},{"constant":false,"inputs":[{"name":"col","type":"uint8"},{"name":"row","type":"uint8"}],"name":"farmTile","outputs":[],"type":"function"},{"constant":true,"inputs":[{"name":"col","type":"uint8"},{"name":"row","type":"uint8"}],"name":"getName","outputs":[{"name":"","type":"string"}],"type":"function"},{"constant":false,"inputs":[{"name":"col","type":"uint8"},{"name":"row","type":"uint8"},{"name":"i","type":"uint8"}],"name":"acceptOffer","outputs":[],"type":"function"},{"constant":false,"inputs":[{"name":"col","type":"uint8"},{"name":"row","type":"uint8"},{"name":"i","type":"uint8"}],"name":"rejectOffer","outputs":[],"type":"function"},{"constant":true,"inputs":[{"name":"col","type":"uint8"},{"name":"row","type":"uint8"}],"name":"getOffers","outputs":[{"name":"","type":"uint256[]"}],"type":"function"},{"constant":true,"inputs":[{"name":"col","type":"uint8"},{"name":"row","type":"uint8"}],"name":"getStatus","outputs":[{"name":"","type":"string"}],"type":"function"},{"constant":true,"inputs":[{"name":"col","type":"uint8"},{"name":"row","type":"uint8"}],"name":"getOwner","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":false,"inputs":[{"name":"col","type":"uint8"},{"name":"row","type":"uint8"}],"name":"retractOffer","outputs":[],"type":"function"},{"constant":true,"inputs":[{"name":"col","type":"uint8"},{"name":"row","type":"uint8"}],"name":"getBlocks","outputs":[{"name":"","type":"int8[5][]"}],"type":"function"},{"inputs":[],"type":"constructor"}];
-var etheria = web3.eth.contract(abi).at('0xe468d26721b703d224d05563cb64746a7a40e1f4');
+var abi = [{"constant":true,"inputs":[],"name":"getWhatHappened","outputs":[{"name":"","type":"uint8"}],"type":"function"},{"constant":false,"inputs":[],"name":"kill","outputs":[],"type":"function"},{"constant":true,"inputs":[{"name":"_b32","type":"bytes32"},{"name":"byteindex","type":"uint8"}],"name":"getUint8FromByte32","outputs":[{"name":"","type":"uint8"}],"type":"function"},{"constant":false,"inputs":[{"name":"col","type":"uint8"},{"name":"row","type":"uint8"},{"name":"_s","type":"string"}],"name":"setStatus","outputs":[],"type":"function"},{"constant":false,"inputs":[{"name":"col","type":"uint8"},{"name":"row","type":"uint8"}],"name":"makeOffer","outputs":[],"type":"function"},{"constant":true,"inputs":[{"name":"col","type":"uint8"},{"name":"row","type":"uint8"}],"name":"getOfferers","outputs":[{"name":"","type":"address[]"}],"type":"function"},{"constant":false,"inputs":[{"name":"col","type":"uint8"},{"name":"row","type":"uint8"},{"name":"i","type":"uint8"},{"name":"amt","type":"uint256"}],"name":"deleteOffer","outputs":[],"type":"function"},{"constant":true,"inputs":[{"name":"col","type":"uint8"},{"name":"row","type":"uint8"}],"name":"getLastFarm","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"col","type":"uint8"},{"name":"row","type":"uint8"},{"name":"index","type":"uint256"},{"name":"_block","type":"int8[5]"}],"name":"editBlock","outputs":[],"type":"function"},{"constant":false,"inputs":[{"name":"col","type":"uint8"},{"name":"row","type":"uint8"},{"name":"_n","type":"string"}],"name":"setName","outputs":[],"type":"function"},{"constant":false,"inputs":[{"name":"col","type":"uint8"},{"name":"row","type":"uint8"}],"name":"farmTile","outputs":[],"type":"function"},{"constant":false,"inputs":[{"name":"col","type":"uint8"},{"name":"row","type":"uint8"},{"name":"i","type":"uint8"},{"name":"amt","type":"uint256"}],"name":"acceptOffer","outputs":[],"type":"function"},{"constant":true,"inputs":[],"name":"isLocked","outputs":[{"name":"","type":"bool"}],"type":"function"},{"constant":true,"inputs":[{"name":"col","type":"uint8"},{"name":"row","type":"uint8"}],"name":"getName","outputs":[{"name":"","type":"string"}],"type":"function"},{"constant":true,"inputs":[{"name":"col","type":"uint8"},{"name":"row","type":"uint8"}],"name":"getOffers","outputs":[{"name":"","type":"uint256[]"}],"type":"function"},{"constant":true,"inputs":[{"name":"col","type":"uint8"},{"name":"row","type":"uint8"}],"name":"getStatus","outputs":[{"name":"","type":"string"}],"type":"function"},{"constant":true,"inputs":[{"name":"col","type":"uint8"},{"name":"row","type":"uint8"}],"name":"getOwner","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":false,"inputs":[],"name":"lock","outputs":[],"type":"function"},{"constant":true,"inputs":[{"name":"col","type":"uint8"},{"name":"row","type":"uint8"}],"name":"getBlocks","outputs":[{"name":"","type":"int8[5][]"}],"type":"function"},{"inputs":[],"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"col","type":"uint8"},{"indexed":false,"name":"row","type":"uint8"}],"name":"TileChanged","type":"event"}]
+var etheria = web3.eth.contract(abi).at('0x429e434f0de48b2a75b7147105582d61bb4ea8ec');
+
+var events = etheria.allEvents(function(error, log){
+	if (error)
+		console.log(error);
+	else if (!error)
+	{
+		console.log(log);
+		retrieveTileInfo (log.args.col*1, log.args.row*1);
+	}
+});
+
+//var events = myContractInstance.allEvents([additionalFilterObject]);
+//
+////watch for changes
+//events.watch(function(error, event){
+//if (!error)
+// console.log(event);
+//});
 
 var elevation_abi = [{"constant":true,"inputs":[],"name":"getElevations","outputs":[{"name":"","type":"uint8[1089]"}],"type":"function"},{"constant":false,"inputs":[],"name":"setLocked","outputs":[],"type":"function"},{"constant":true,"inputs":[],"name":"getLocked","outputs":[{"name":"","type":"bool"}],"type":"function"},{"constant":true,"inputs":[{"name":"col","type":"uint8"},{"name":"row","type":"uint8"}],"name":"getElevation","outputs":[{"name":"","type":"uint8"}],"type":"function"},{"constant":false,"inputs":[{"name":"col","type":"uint8"},{"name":"_elevations","type":"uint8[33]"}],"name":"initElevations","outputs":[],"type":"function"}];
 var elevationaddress = "0x68549d7dbb7a956f955ec1263f55494f05972a6b";
@@ -62,6 +79,8 @@ elevationcontract.getElevations.call(function(error, result) {
 });
 console.log('done getting elevations');
 
+//owner, name, status, lastfarm, offerers, offers, blocks
+
 var owners;
 owners = new Array(mapsize);
 for (i = 0; i < mapsize; i++) {
@@ -73,10 +92,29 @@ names = new Array(mapsize);
 for (i = 0; i < mapsize; i++) {
 	  names[i] = new Array(mapsize);
 }
+
 var statuses;
 statuses = new Array(mapsize);
 for (i = 0; i < mapsize; i++) {
 	  statuses[i] = new Array(mapsize);
+}
+
+var lastfarms;
+lastfarms = new Array(mapsize);
+for (i = 0; i < mapsize; i++) {
+	  lastfarms[i] = new Array(mapsize);
+}
+
+var offerers;
+offerers = new Array(mapsize);
+for (i = 0; i < mapsize; i++) {
+	  offerers[i] = new Array(mapsize);
+}
+
+var offers;
+offers = new Array(mapsize);
+for (i = 0; i < mapsize; i++) {
+	  offers[i] = new Array(mapsize);
 }
 
 var blocks;
@@ -85,36 +123,23 @@ for (i = 0; i < mapsize; i++) {
 	  blocks[i] = new Array(mapsize);
 }
 
-var map_retrieval_index = 0;
-retrieveTileInfo();
-setInterval(function () { retrieveTileInfo(map_retrieval_index); map_retrieval_index++; if(map_retrieval_index === Math.pow(mapsize,2)) { map_retrieval_index = 0; }}, 250); // map refresh every 3.63 minutes
-function retrieveTileInfo ( row ) {
+//var map_retrieval_index = 0;
+//
+//retrieveTileInfo(map_retrieval_index);
+//map_retrieval_index++;
+//
+//setInterval(function () { retrieveTileInfo(map_retrieval_index); map_retrieval_index++; if(map_retrieval_index === Math.pow(mapsize,2)) { map_retrieval_index = 0; }}, 500);
+
+function retrieveTileInfo (col, row) {
 	var batch = web3.createBatch();
 	
-	row = map_retrieval_index % 33;
-	col = (map_retrieval_index - row) / mapsize;
-	//console.log('getting block for ' + col + "," + row);
+	//row = map_retrieval_index % 33;
+	//col = (map_retrieval_index - row) / mapsize;
+	console.log('getting block for ' + col + "," + row);
 	
 		var gB = function() { 
 			var c = col;
 			var r = row;
-			batch.add(etheria.getBlocks.request(c, r, function(error, result) { //console.log('returned from getting blocks for ' + c + "," + r + JSON.stringify(result)); blocks[c][r] = result; });
-				if(typeof result === "undefined" || result === null)
-				{
-					console.log('WARNING: Retrieval of blocks at c,r ' + c + "," + r + 'failed');
-				}	
-				else
-				{
-					for(var u = 0; u < result.length; u++)
-					{
-						for(var v = 0; v < 5; v++)
-						{
-							result[u][v] = result[u][v]*1; 
-						}	
-					}
-					blocks[c][r] = result;
-				}
-			}));
 			batch.add(etheria.getOwner.request(c, r, function(error, result) { //console.log('returned from getting blocks for ' + c + "," + r + JSON.stringify(result)); blocks[c][r] = result; });
 				if(typeof result === "undefined" || result === null)
 				{
@@ -145,10 +170,139 @@ function retrieveTileInfo ( row ) {
 					statuses[c][r] = result;
 				}
 			}));
+			batch.add(etheria.getLastFarm.request(c, r, function(error, result) { //console.log('returned from getting blocks for ' + c + "," + r + JSON.stringify(result)); blocks[c][r] = result; });
+				if(typeof result === "undefined" || result === null)
+				{
+					console.log('WARNING: Retrieval of lastfarm c,r ' + c + "," + r + 'failed');
+				}	
+				else
+				{
+					for(var u = 0; u < result.length; u++)
+					{
+						for(var v = 0; v < 5; v++)
+						{
+							result[u][v] = result[u][v]*1; 
+						}	
+					}
+					lastfarms[c][r] = result;
+				}
+			}));
+			batch.add(etheria.getOfferers.request(c, r, function(error, result) { //console.log('returned from getting blocks for ' + c + "," + r + JSON.stringify(result)); blocks[c][r] = result; });
+				if(typeof result === "undefined" || result === null)
+				{
+					console.log('WARNING: Retrieval of offerers c,r ' + c + "," + r + 'failed');
+				}	
+				else
+				{
+					offerers[c][r] = result;
+				}
+			}));
+			batch.add(etheria.getOffers.request(c, r, function(error, result) { //console.log('returned from getting blocks for ' + c + "," + r + JSON.stringify(result)); blocks[c][r] = result; });
+				if(typeof result === "undefined" || result === null)
+				{
+					console.log('WARNING: Retrieval of offers c,r ' + c + "," + r + 'failed');
+				}	
+				else
+				{
+					for(var u = 0; u < result.length; u++)
+					{
+						for(var v = 0; v < 5; v++)
+						{
+							result[u][v] = result[u][v]*1; 
+						}	
+					}
+					offers[c][r] = result;
+				}
+			}));
+			batch.add(etheria.getBlocks.request(c, r, function(error, result) { //console.log('returned from getting blocks for ' + c + "," + r + JSON.stringify(result)); blocks[c][r] = result; });
+				if(typeof result === "undefined" || result === null)
+				{
+					console.log('WARNING: Retrieval of blocks at c,r ' + c + "," + r + 'failed');
+				}	
+				else
+				{
+					for(var u = 0; u < result.length; u++)
+					{
+						for(var v = 0; v < 5; v++)
+						{
+							result[u][v] = result[u][v]*1; 
+						}	
+					}
+					blocks[c][r] = result;
+				}
+			}));
 		};
 		gB();
 	batch.execute();
 }
+//function retrieveTileInfo () 
+//{
+//var batch = web3.createBatch();
+//
+////row = map_retrieval_index % 33;
+////col = (map_retrieval_index - row) / mapsize;
+////console.log('getting block for ' + col + "," + row);
+//
+//for(var col = 0; col < (mapsize -1); col++)
+//{
+//	for(var row = 0; row < (mapsize -1); row++)
+//	{
+//		var gB = function() { 
+//			var c = col;
+//			var r = row;
+//			batch.add(etheria.getBlocks.request(c, r, function(error, result) { //console.log('returned from getting blocks for ' + c + "," + r + JSON.stringify(result)); blocks[c][r] = result; });
+//				if(typeof result === "undefined" || result === null)
+//				{
+//					console.log('WARNING: Retrieval of blocks at c,r ' + c + "," + r + 'failed');
+//				}	
+//				else
+//				{
+//					for(var u = 0; u < result.length; u++)
+//					{
+//						for(var v = 0; v < 5; v++)
+//						{
+//							result[u][v] = result[u][v]*1; 
+//						}	
+//					}
+//					blocks[c][r] = result;
+//				}
+//			}));
+//			batch.add(etheria.getOwner.request(c, r, function(error, result) { //console.log('returned from getting blocks for ' + c + "," + r + JSON.stringify(result)); blocks[c][r] = result; });
+//				if(typeof result === "undefined" || result === null)
+//				{
+//					console.log('WARNING: Retrieval of owner c,r ' + c + "," + r + 'failed');
+//				}	
+//				else
+//				{
+//					owners[c][r] = result;
+//				}
+//			}));
+//			batch.add(etheria.getName.request(c, r, function(error, result) { //console.log('returned from getting blocks for ' + c + "," + r + JSON.stringify(result)); blocks[c][r] = result; });
+//				if(typeof result === "undefined" || result === null)
+//				{
+//					console.log('WARNING: Retrieval of name c,r ' + c + "," + r + 'failed');
+//				}	
+//				else
+//				{
+//					names[c][r] = result;
+//				}
+//			}));
+//			batch.add(etheria.getStatus.request(c, r, function(error, result) { //console.log('returned from getting blocks for ' + c + "," + r + JSON.stringify(result)); blocks[c][r] = result; });
+//				if(typeof result === "undefined" || result === null)
+//				{
+//					console.log('WARNING: Retrieval of status c,r ' + c + "," + r + 'failed');
+//				}	
+//				else
+//				{
+//					statuses[c][r] = result;
+//				}
+//			}));
+//		};
+//		gB();
+//	}
+//}
+//batch.execute();
+//}
 
 /* we want to create a 2D array where [1][3] gets the 4th object in the array at the 2nd outer array position
 	[
