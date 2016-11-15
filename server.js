85618a99080f0b436dd61ef7922cb4d958179a1f
cyrus <github@mailcyr.us>
2016-11-15 09:34:52 -0500

    Updated web3 change.

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 var express = require('express');
+var Web3 = require('web3');
 
-var web3 = require('web3');
-if (typeof web3 !== 'undefined') 
-{
-	web3 = new Web3(web3.currentProvider);
-} else {	
-	// set the provider you want from Web3.providers
-	web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-}
-//web3.setProvider(new web3.providers.HttpProvider('http://127.0.0.1:8545'));
+var web3 = new Web3();
+
+console.log("web3 is NOT null");
+console.log(JSON.stringify(web3));
+web3.setProvider(new web3.providers.HttpProvider('http://127.0.0.1:8545'));
 
 var elevation_abi = [{"constant":true,"inputs":[],"name":"getElevations","outputs":[{"name":"","type":"uint8[1089]"}],"type":"function"},{"constant":false,"inputs":[],"name":"setLocked","outputs":[],"type":"function"},{"constant":true,"inputs":[],"name":"getLocked","outputs":[{"name":"","type":"bool"}],"type":"function"},{"constant":true,"inputs":[{"name":"col","type":"uint8"},{"name":"row","type":"uint8"}],"name":"getElevation","outputs":[{"name":"","type":"uint8"}],"type":"function"},{"constant":false,"inputs":[],"name":"kill","outputs":[],"type":"function"},{"constant":false,"inputs":[{"name":"col","type":"uint8"},{"name":"_elevations","type":"uint8[33]"}],"name":"initElevations","outputs":[],"type":"function"},{"inputs":[],"type":"constructor"}]
 var elevationcontract = web3.eth.contract(elevation_abi).at("0x68549d7dbb7a956f955ec1263f55494f05972a6b");
@@ -218,74 +215,6 @@ function retrieveTileInfo (col, row) {
 		gB();
 //	batch.execute();
 }
-//function retrieveTileInfo () 
-//{
-//var batch = web3.createBatch();
-//
-////row = map_retrieval_index % 33;
-////col = (map_retrieval_index - row) / mapsize;
-////console.log('getting block for ' + col + "," + row);
-//
-//for(var col = 0; col < (mapsize -1); col++)
-//{
-//	for(var row = 0; row < (mapsize -1); row++)
-//	{
-//		var gB = function() { 
-//			var c = col;
-//			var r = row;
-//			batch.add(etheria.getBlocks.request(c, r, function(error, result) { //console.log('returned from getting blocks for ' + c + "," + r + JSON.stringify(result)); blocks[c][r] = result; });
-//				if(typeof result === "undefined" || result === null)
-//				{
-//					console.log('WARNING: Retrieval of blocks at c,r ' + c + "," + r + 'failed');
-//				}	
-//				else
-//				{
-//					for(var u = 0; u < result.length; u++)
-//					{
-//						for(var v = 0; v < 5; v++)
-//						{
-//							result[u][v] = result[u][v]*1; 
-//						}	
-//					}
-//					blocks[c][r] = result;
-//				}
-//			}));
-//			batch.add(etheria.getOwner.request(c, r, function(error, result) { //console.log('returned from getting blocks for ' + c + "," + r + JSON.stringify(result)); blocks[c][r] = result; });
-//				if(typeof result === "undefined" || result === null)
-//				{
-//					console.log('WARNING: Retrieval of owner c,r ' + c + "," + r + 'failed');
-//				}	
-//				else
-//				{
-//					owners[c][r] = result;
-//				}
-//			}));
-//			batch.add(etheria.getName.request(c, r, function(error, result) { //console.log('returned from getting blocks for ' + c + "," + r + JSON.stringify(result)); blocks[c][r] = result; });
-//				if(typeof result === "undefined" || result === null)
-//				{
-//					console.log('WARNING: Retrieval of name c,r ' + c + "," + r + 'failed');
-//				}	
-//				else
-//				{
-//					names[c][r] = result;
-//				}
-//			}));
-//			batch.add(etheria.getStatus.request(c, r, function(error, result) { //console.log('returned from getting blocks for ' + c + "," + r + JSON.stringify(result)); blocks[c][r] = result; });
-//				if(typeof result === "undefined" || result === null)
-//				{
-//					console.log('WARNING: Retrieval of status c,r ' + c + "," + r + 'failed');
-//				}	
-//				else
-//				{
-//					statuses[c][r] = result;
-//				}
-//			}));
-//		};
-//		gB();
-//	}
-//}
-//batch.execute();
-//}
 
 /* we want to create a 2D array where [1][3] gets the 4th object in the array at the 2nd outer array position
 	[
